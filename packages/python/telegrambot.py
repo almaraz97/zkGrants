a4cde70a41e8bca32ddcf9a36bfae8bee1f1fa35
Alejandro Almaraz <54364825+almaraz97@users.noreply.github.com>
2024-09-14 16:00:15 -0300

    add working tx flow for the tg bot, refactor contract instantiation, reply with transaction link

--- packages/python/telegrambot.py
+++ packages/python/telegrambot.py
@@ -10,32 +10,30 @@ TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 ETH_NODE_URL = os.getenv("SEPOLIA_RPC_URL")
 PRIVATE_KEY = os.getenv("PRIVATE_KEY")
 SEMAPHORE_CONTRACT_ADDRESS = "0x1e0d7FF1610e480fC93BdEC510811ea2Ba6d7c2f" # os.getenv("SEMAPHORE_CONTRACT_ADDRESS")
-CONTRACT_ABI_PATH = os.getcwd() + "/packages/python/Semaphore.json"
-GAS_LIMIT = int(os.getenv("GAS_LIMIT", "300000"))
-GROUP_ID = 33 # Hardcoded for now # https://sepolia.etherscan.io/tx/0xcbff2e65af446fef30f0df2e79d2d50495e39ddb7c4509679fe59fe173244a14
+CONTRACT_ABI_PATH = os.getcwd() + "/Semaphore.json"
+GAS_LIMIT = int(os.getenv("GAS_LIMIT", "3000000"))
+GROUP_ID = 51 # Hardcoded for now
 
-# Load Contract ABI
 with open(CONTRACT_ABI_PATH, 'r') as abi_file:
     contract_abi = json.load(abi_file)
 
-# Initialize Web3
 web3 = Web3(Web3.HTTPProvider(ETH_NODE_URL))
-
+contract = web3.eth.contract(address=SEMAPHORE_CONTRACT_ADDRESS, abi=contract_abi)
 
 def get_nonce(address):
     return web3.eth.get_transaction_count(address)
 
 def send_contract_transaction(function_name, *args):
     account = web3.eth.account.from_key(PRIVATE_KEY)
-    tx = web3.eth.contract(address=SEMAPHORE_CONTRACT_ADDRESS, abi=contract_abi).functions[function_name](*args).build_transaction({
+    tx = contract.functions[function_name](*args).build_transaction({
         'from': account.address,
         'nonce': get_nonce(account.address),
         'gas': GAS_LIMIT,
-        'gasPrice': web3.eth.gas_price
+        'gasPrice': int(web3.eth.gas_price * 1.5)
     })
     signed_tx = web3.eth.account.sign_transaction(tx, PRIVATE_KEY)
-    tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
-    return web3.toHex(tx_hash)
+    tx_hash = web3.eth.send_raw_transaction(signed_tx.raw_transaction)
+    return web3.to_hex(tx_hash)
 
 bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
 
@@ -48,7 +46,7 @@ def add_member_handler(message):
     try:
         identifier = int(message.text)
         tx_hash = send_contract_transaction('addMember', GROUP_ID, identifier)
-        bot.reply_to(message, f"Transaction sent to add member {identifier}! Hash: {tx_hash}")
+        bot.reply_to(message, f"You've been added! Tx: https://sepolia.etherscan.io/tx/{tx_hash}")
     except Exception as e:
         bot.reply_to(message, f"Error: {str(e)}")
 
