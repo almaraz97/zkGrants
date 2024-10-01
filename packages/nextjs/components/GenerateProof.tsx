0ec5e86e0ddf52c5a4d2f3a8fc871f4e3e1a8160
Alejandro Almaraz <54364825+almaraz97@users.noreply.github.com>
2024-10-01 16:48:27 -0300

    add casting of vote onchain using abi encoded proof with the contract call

--- packages/nextjs/components/GenerateProof.tsx
+++ packages/nextjs/components/GenerateProof.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import { FC } from "react";
-import { Group, Identity, generateProof } from "@semaphore-protocol/core";
-// import { SemaphoreSubgraph } from "@semaphore-protocol/data";
+import { Group, Identity, SemaphoreProof, generateProof } from "@semaphore-protocol/core";
+import { writeContract } from "@wagmi/core";
+import { Address, encodeAbiParameters } from "viem";
 import { CheckIcon } from "@heroicons/react/24/outline";
-
-// import { http, createConfig, writeContract } from '@wagmi/core'
-// import { sepolia } from '@wagmi/core/chains'
+import { wagmiConfig } from "~~/services/web3/wagmiConfig";
 
 interface GenerateProofProps {
   group: Group | undefined;
@@ -25,36 +24,84 @@ const GenerateProof: FC<GenerateProofProps> = ({ group, identity, grantId, group
     console.log(identity, groupId);
 
     const message = 1;
-    const proof = await generateProof(identity, group, message, grantId);
+    const proof: SemaphoreProof = await generateProof(identity, group, message, grantId);
     console.log(proof);
 
-    // const config = createConfig({
-    //   chains: [sepolia],
-    //   transports: {
-    //     [sepolia.id]: http(),
-    //   },
-    // })
+    if (!proof) return;
+    const unpackedProofPoints: [bigint, bigint, bigint, bigint, bigint, bigint, bigint, bigint] = [
+      BigInt(proof.points[0]),
+      BigInt(proof.points[1]),
+      BigInt(proof.points[3]),
+      BigInt(proof.points[2]),
+      BigInt(proof.points[5]),
+      BigInt(proof.points[4]),
+      BigInt(proof.points[6]),
+      BigInt(proof.points[7]),
+    ];
 
-    // const result = await writeContract(config, {
-    //   abi: [], // TODO add denota abi
-    //   address: '0x00002bCC9B3e92a59207C43631f3b407AE5bBd0B',  // denotaSDK.denotaContracts["registrar"].address
-    //   functionName: 'cash',
-    //   args: [
-    //     BigInt(grantId),
-    //     BigInt(100), // TODO compute how much can be cashed
-    //     `0x${notaOwner}`, // to
-    //     // Need to abi.encode this
-    //     // encodeAbiParameters(
-    //     //   [
-    //     //     { name: 'merkleTreeDepth', type: 'uint256' },
-    //     //     { name: 'merkleTreeRoot', type: 'uint256' },
-    //     //     // { name: 'z', type: 'bool' }
-    //     //   ],
-    //     //   // [BigInt(proof.merkleTreeDepth), proof.merkleTreeRoot, proof.nullifier, proof.scope, proof.points]
-    //     // ),
-    //     `0x{proof}`, // TODO insert proof here
-    //   ],
-    // })
+    const encodedProof = encodeAbiParameters(
+      // hookData
+      [
+        { name: "merkleTreeDepth", type: "uint256" },
+        { name: "merkleTreeRoot", type: "uint256" },
+        { name: "nullifier", type: "uint256" },
+        { name: "message", type: "uint256" },
+        { name: "scope", type: "uint256" },
+        { name: "points", type: "uint256[8]" },
+      ],
+      [
+        BigInt(proof.merkleTreeDepth),
+        BigInt(proof.merkleTreeRoot),
+        BigInt(proof.nullifier),
+        BigInt(proof.message),
+        BigInt(proof.scope),
+        unpackedProofPoints,
+      ],
+    );
+    console.log(encodedProof);
+
+    const result = await writeContract(wagmiConfig, {
+      abi: [
+        {
+          inputs: [
+            {
+              internalType: "uint256",
+              name: "notaId",
+              type: "uint256",
+            },
+            {
+              internalType: "uint256",
+              name: "amount",
+              type: "uint256",
+            },
+            {
+              internalType: "address",
+              name: "to",
+              type: "address",
+            },
+            {
+              internalType: "bytes",
+              name: "hookData",
+              type: "bytes",
+            },
+          ],
+          name: "cash",
+          outputs: [],
+          stateMutability: "payable",
+          type: "function",
+        },
+      ], // TODO add denota abi
+      address: "0x00002bCC9B3e92a59207C43631f3b407AE5bBd0B", // denotaSDK.contractMappingForChainId(chainName).registrar
+      functionName: "cash",
+      args: [
+        BigInt(grantId), // notaId
+        BigInt(100), // amount // TODO auto-compute how much can be cashed
+        notaOwner as Address, // to
+        encodedProof,
+      ],
+    });
+
+    window.alert(`Vote casted: ${result}`);
   };
 
   return (
